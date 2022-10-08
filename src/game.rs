8de4275d169ea5ad5b37b1cc42a2f76c9f9bd006
Fernando Silveira <fsilveira@gmail.com>
2022-10-08 14:19:39 -0300

    Implemented the main loop

--- src/game.rs
+++ src/game.rs
@@ -22,6 +22,8 @@ impl Game {
 
         let video_subsystem = self.sdl_context.video()?;
 
+        let timer = self.sdl_context.timer()?;
+
         let _window = video_subsystem
             .window(self.name, 640, 480)
             .position_centered()
@@ -30,32 +32,70 @@ impl Game {
 
         let mut event_pump = self.sdl_context.event_pump()?;
 
-        let mut running = true;
-        while running {
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. } => {
-                        running = false;
-                    }
-                    Event::KeyUp { keycode, repeat, .. } => {
-                        if repeat {
-                            break;
-                        }
-                        println!("{} Up", keycode.unwrap());
-                    }
-                    Event::KeyDown { keycode, repeat, .. } => {
-                        if repeat {
-                            break;
-                        }
-                        println!("{} Down", keycode.unwrap());
-                    }
-                    _ => {}
+        let update_period = 16;
+        let minimum_render_period = 16;
+        let maximum_render_period = 1000;
+
+        let offset_tick = timer.ticks();
+
+        let mut current_tick = 0;
+        let mut last_update_tick = 0;
+        let mut last_render_tick = 0;
+
+        self.update(last_update_tick);
+        self.render();
+
+        'main : loop {
+            let next_render = last_render_tick + minimum_render_period;
+            let next_render_limit = last_render_tick + maximum_render_period;
+
+            loop {
+                let timeout = if next_render <= current_tick { 0 } else { next_render - current_tick };
+                let wait_event = event_pump.wait_event_timeout(timeout);
+                current_tick = timer.ticks() - offset_tick;
+
+                while (last_update_tick + update_period) <= current_tick {
+                    last_update_tick += update_period;
+                    self.update(last_update_tick);
+                }
+
+                let event = match wait_event {
+                    None => break,
+                    Some(Event::Quit { .. }) => break 'main,
+                    Some(event) => event,
+                };
+
+                self.handle_event(last_update_tick, event);
+
+                current_tick = timer.ticks() - offset_tick;
+                if current_tick >= next_render_limit {
+                    break;
                 }
             }
+
+            if last_render_tick != last_update_tick {
+                self.render();
+                last_render_tick = last_update_tick;
+            }
+
+            current_tick = timer.ticks() - offset_tick;
+
         }
 
         println!("Goodbye, world.");
 
         Ok(())
+    }
+
+    fn handle_event(&self, last_update_tick: u32, _event: Event) {
+        println!("handle_event({last_update_tick}, <event>);");
+    }
+
+    fn update(&self, millis: u32) {
+        println!("update({millis});");
+    }
+
+    fn render(&self) {
+        println!("render();");
     }
 }
